adfaaeabbabc4be9f05a5b74ff8c298a0e7998a4
DavidCh <david.chandra0501@gmail.com>
2024-01-04 20:14:52 +0700

    testing

--- gittrackr/settings.py
+++ gittrackr/settings.py
@@ -19,8 +19,7 @@ GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -140,8 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
